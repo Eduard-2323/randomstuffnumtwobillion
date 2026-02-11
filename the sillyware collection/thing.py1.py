3d0d8c36c1d82f3a959e81e829a991b8b37a7ccf
Eduard-2323 <carmenbizineche0@gmail.com>
2026-02-11 14:52:34 +0200

    actualy started doing something

--- the sillyware collection/thing.py1.py
+++ the sillyware collection/thing.py1.py
@@ -1,6 +1,16 @@
 from tkinter import messagebox
-ison = messagebox.askquestion("warningmessage","are ya shure ya wanna do this?")
+import pyautogui
+import random
+import time
+ison = messagebox.askquestion("warningmessage","are ya shure ya wanna do this? \n this is preaty mutch just annoying malware")
+
+def the_actual_thing():
+    while True:
+        time.sleep(0.1)
+        pyautogui.move(random.randint(-100,100),random.randint(-100,100))
+
 if ison == "yes":
-    print("started")
+    print("yippe")
+    the_actual_thing()
 else:
     print("stopped")
